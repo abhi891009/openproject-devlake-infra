d279ccca2e1fb6b98c1520b6c7b5575c63012dfc
abhi891009 <33829011+abhi891009@users.noreply.github.com>
2025-05-10 21:26:01 +0000

    Commiting the changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,10 +51,10 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
