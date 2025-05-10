8b6c153c88303fee616f7f3ae91f769b657be866
abhi891009 <33829011+abhi891009@users.noreply.github.com>
2025-05-10 22:41:23 +0000

    Commiting the changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -120,7 +120,7 @@ resource "aws_instance" "ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids = [aws_security_group.ec2_sg.name]
   key_name      = aws_key_pair.deployer.key_name
 
   user_data = <<-EOF
