d434279af4b20ac90d9fffe8a4fe512fa10efc14
abhi891009 <33829011+abhi891009@users.noreply.github.com>
2025-05-12 08:17:21 +0000

    Commiting the changes CI/CD pipeline

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,10 +5,10 @@ output "alb_dns_name" {
 output "private_key_pem" {
   value     = tls_private_key.ssh_key.private_key_pem
   sensitive = true
-}
-output "ec2_public_ip" {
-  value = aws_instance.ec2.public_ip
-}
+#}
+#output "ec2_public_ip" {
+ # value = aws_instance.ec2.public_ip
+#}
 
 output "ec2_public_ip" {
   value = aws_eip.static_ip.public_ip
