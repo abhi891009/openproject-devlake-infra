de0660deca8eeae8730b0722b8444dde6c14d15d
abhi891009 <33829011+abhi891009@users.noreply.github.com>
2025-05-13 06:06:22 +0000

    Ci/CD Pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -109,7 +109,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port = 8081
     to_port   = 8081
     protocol  = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
